f85e52ac18b0be6a9a208bf0abb220db2d5d81a9
774222117 <774222117@qq.com>
2022-09-30 17:55:32 +0800

    20220930

--- VUE2/路由/App.vue
+++ VUE2/路由/App.vue
@@ -1,15 +1,73 @@
 <template>
 	<div id="app">
-		<router-link to="/home">首页</router-link>
-		<router-link to="/about">关于</router-link>
+		<!-- tag=button 表示将router-link渲染成button样式 -->
+		<!-- replace  取消网站返回和前进按钮 -->
+		<!-- active-class="active" 修改默认的选中样式-->
+		<!-- <router-link to="/home" tag="button" replace active-class="active">首页</router-link> -->
+		<!-- <router-link to="/about" tag="button" replace active-class="active">关于</router-link> -->
+		<router-link to="/home" tag="button" replace>首页</router-link>
+		<router-link to="/about" tag="button" replace>关于</router-link>
+		<router-link :to="'/user/'+userId" tag="button" replace>用户</router-link>
+		<router-link :to="{path:'/profile',query:{name:'why',age:'18',height:1.88}}">档案</router-link>
+		<!-- router-view绝对展示的位置 -->
+		<router-view></router-view>
+		
+		
+		<!-- 点击按钮跳转路由页面 -->
+		<button @click="homeClick">首页</button>
+		<button @click="aboutClick">关于</button>
+		
+		
+		<button @click="userClick">用户</button>
+		<button @click="profileClick">档案</button>
+		
+		
 	</div>
 </template>
 
 <script>
 	export default {
-		name:'App'
+		name:'App',
+		data(){
+			return {
+				userId:'zhangsan'
+			}
+		},
+		methods:{
+			// 通过代码的方式修改路由 vue-router
+			homeClick(){
+				// 两种方法都可以
+				// this.$router.push('/home')
+				this.$router.replace('/home')
+			},
+			aboutClick(){
+				// this.$router.push('/about')
+				this.$router.replace('/about')
+			},
+			userClick(){
+				this.$router.push('/user' + this.userId)
+			},
+			profileClick(){
+				this.$router.push({
+					path:'/profile',
+					query:{
+						name:'Kobe',
+						age:18,
+						height:1.88
+					}
+				})
+			}
+		}
 	}
 </script>
 
 <style>
+	/* 被点击的样式改变 */
+	.router-link-active {
+		color:red
+	}
+	
+	.active {
+		color: red;
+	}
 </style>
---
+++ VUE2/路由/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+	<h2>我是用户界面</h2>
+	<p>我是用户的相关信息，XXX</p>
+	<h2>{{userId}}</h2>
+	<h2>{{$route.params.id}}</h2>
+</template>
+
+<script>
+	export default {
+		name:"User",
+		computed:{
+			userId(){
+				// 哪个路由处于活跃 $route就是指那个
+				return this.$route.params.id
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
